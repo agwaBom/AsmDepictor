e5d91d9a046a1734906f6007cb5370a6335517b9
agwaBom <khyunjin1993@gmail.com>
2022-05-23 18:29:02 +0900

    working on argparse

--- main_torchtext.py
+++ main_torchtext.py
@@ -8,6 +8,7 @@ from tqdm import tqdm
 import math
 import json
 import random
+import argparse
 import os
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 
@@ -15,13 +16,16 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 import pandas as pd
 from torchtext.data import Field, BucketIterator, TabularDataset
 from sklearn.utils import shuffle
-from sklearn.metrics import precision_recall_fscore_support
+
+# Model
 import model.asmdepictor.Models as Asmdepictor
 from model.asmdepictor.Translator import Translator
 from model.asmdepictor.Optim import ScheduledOptim
-from random_seed import set_random_seed
 
-set_random_seed(123)
+# Metrics
+from random_seed import set_random_seed
+from sklearn.metrics import precision_recall_fscore_support
+
 
 
 def cal_performance(pred, pred_sentence, gold, gold_sentence, trg_pad_idx, smoothing=False):
@@ -94,7 +98,7 @@ def cal_loss(pred, gold, trg_pad_idx, smoothing=False):
         loss = F.cross_entropy(pred, gold, ignore_index=trg_pad_idx, reduction='sum')
     return loss
 
-def train(input_tensor, target_tensor, model, model_optimizer, smoothing):
+def train(input_tensor, target_tensor, model, model_optimizer, smoothing, trg_pad_idx):
     total_loss, n_word_total, n_word_correct = 0, 0, 0 
 
     model_optimizer.zero_grad()
@@ -117,7 +121,7 @@ def train(input_tensor, target_tensor, model, model_optimizer, smoothing):
 
     return loss.item(), n_correct, n_word, f1
 
-def validate(input_tensor, target_tensor, model):
+def validate(input_tensor, target_tensor, model, trg_pad_idx):
     total_loss, n_word_total, n_word_correct = 0, 0, 0
 
     target_tensor = target_tensor.transpose(0, 1)
@@ -134,7 +138,7 @@ def validate(input_tensor, target_tensor, model):
 
     return loss.item(), n_correct, n_word, f1
 
-def trainIters(model, n_epoch, train_iterator, test_iterator, model_optimizer, smoothing):
+def trainIters(model, n_epoch, train_iterator, test_iterator, model_optimizer, smoothing, trg_pad_idx):
     for i in range(1, n_epoch + 1):
         total_train_f1 = 0
         total_train_prec = 0
@@ -149,7 +153,7 @@ def trainIters(model, n_epoch, train_iterator, test_iterator, model_optimizer, s
         for batch in tqdm(train_iterator):
             input_tensor = batch.code
             target_tensor = batch.text
-            train_loss, n_correct, n_word, f1 = train(input_tensor, target_tensor, model, model_optimizer, smoothing)
+            train_loss, n_correct, n_word, f1 = train(input_tensor, target_tensor, model, model_optimizer, smoothing, trg_pad_idx)
             total_train_prec += f1[0]
             total_train_rec += f1[1]
             total_train_f1 += f1[2]
@@ -185,7 +189,7 @@ def trainIters(model, n_epoch, train_iterator, test_iterator, model_optimizer, s
             for batch in tqdm(test_iterator):
                 input_tensor = batch.code
                 target_tensor = batch.text
-                target_loss, n_correct, n_word, f1 = validate(input_tensor, target_tensor, model)
+                target_loss, n_correct, n_word, f1 = validate(input_tensor, target_tensor, model, trg_pad_idx)
                 total_valid_prec += f1[0]
                 total_valid_rec += f1[1]
                 total_valid_f1 += f1[2]
@@ -224,7 +228,7 @@ def sentence_to_tensor(sentence, model_type, src_or_tgt):
     pad_idx = code.vocab.stoi[code.pad_token]
     sentence_idx = [code.vocab.stoi.get(i, unk_idx) for i in sentence]
 
-    for i in range(max_token_seq_len-len(sentence_idx)):
+    for i in range(opt.max_token_seq_len-len(sentence_idx)):
         sentence_idx.append(code.vocab.stoi.get(i, pad_idx))
 
     sentence_tensor = torch.tensor(sentence_idx).to(device)
@@ -237,7 +241,7 @@ def make_a_hypothesis_transformer(model, src, tgt):
     translator = Translator(
         model=model,
         beam_size=5,
-        max_seq_len=max_token_seq_len+3,
+        max_seq_len=opt.max_token_seq_len+3,
         src_pad_idx=code.vocab.stoi['<pad>'],
         trg_pad_idx=text.vocab.stoi['<pad>'],
         trg_bos_idx=text.vocab.stoi['<sos>'],
@@ -260,7 +264,7 @@ def make_hypothesis_reference(model, test_src, test_tgt, model_type):
 def random_choice_from_train():
     # read json files made from torchtext
     train_data = list()
-    for line in open("./ours_clean_nero_test/valid.json", mode='r', encoding='utf-8'):
+    for line in open("./dataset/valid.json", mode='r', encoding='utf-8'):
         train_data.append(json.loads(line))
 
     train_data = random.choice(train_data)
@@ -286,69 +290,86 @@ def preprocessing(src_file, tgt_file, max_token_seq_len):
 
     return shuffle(df)
 
+def main():
+    parser = argparse.ArgumentParser()
 
-if __name__ == '__main__':
+    #parser.add_argument('-train_path', default=None, required=True)
+    #parser.add_argument('-val_path', default=None, required=True)
+    #parser.add_argument('-test_path', default=None, required=True)
+
+    parser.add_argument('-batch_size', type=int, default=90)
+    parser.add_argument('-epoch', type=int, default=150)
+    parser.add_argument('-lr_mul', type=float, default=1.0)
+    parser.add_argument('-d_inner_hid', type=int, default=2048)
+    parser.add_argument('-d_k', type=int, default=64)
+    parser.add_argument('-d_v', type=int, default=64)
+    parser.add_argument('-d_model', type=int, default=2048)
+    parser.add_argument('-d_word_vec', type=int, default=512)
+    parser.add_argument('-embs_share_weight', type=bool, default=True)
+    parser.add_argument('-max_token_seq_len', type=int, default=300)
+    parser.add_argument('-n_head', type=int, default=8)
+    parser.add_argument('-n_layers', type=int, default=3)
+    parser.add_argument('-proj_share_weight', type=bool, default=True)
+    parser.add_argument('-scale_emb_or_prj', type=str, default='emb')
+    parser.add_argument('-n_warmup_steps', type=int, default=18000)
+    parser.add_argument('-smoothing', type=bool, default=True)
+    #parser.add_argument('-output_path', default=None, required=True)
+    parser.add_argument('-seed', type=int, default=123)
+    parser.add_argument('-tensorboard_path', default='./runs/asmdepictor')
+
+    global opt
+    opt = parser.parse_args()
+
     # Cuda setting
+    global device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    set_random_seed(opt.seed)
 
     # Hyperparameters
-    n_epoch = 150
-    batch_size = 90
-    d_inner_hid = 2048
-    d_k = 64 
-    d_model = 512 
-    d_v = 64
-    d_word_vec = 512
-    dropout = 0.1
-    embs_share_weight = True
-    global max_token_seq_len
-    max_token_seq_len = 300
-    n_head = 8 
-    n_layers = 3
-    proj_share_weight = True
-    scale_emb_or_prj = 'emb'
+    #n_epoch = 150
+    #batch_size = 90
+    #d_inner_hid = 2048
+    #d_k = 64 
+    #d_model = 512 
+    #d_v = 64
+    #d_word_vec = 512
+    #dropout = 0.1
+    #embs_share_weight = True
+    #global max_token_seq_len
+    #max_token_seq_len = 300
+    #n_head = 8 
+    #n_layers = 3
+    #proj_share_weight = True
+    #scale_emb_or_prj = 'emb'
 
-    lr_mul = 1.0
-    n_warmup_steps = 18000
+    #lr_mul = 1.0
+    #n_warmup_steps = 18000
 
-    smoothing = True
-    
-    writer = SummaryWriter('./runs/train_new_asmdepictor')
+    #smoothing = True
 
-    train_src_dir = "./ours_clean_nero_test/train_source.txt"
-    valid_src_dir = "./ours_clean_nero_test/test_source.txt"
-    test_src_dir = "./ours_clean_nero_test/test_source.txt"
+    global writer
+    writer = SummaryWriter(opt.tensorboard_path)
 
-    train_tgt_dir = "./ours_clean_nero_test/train_target.txt"
-    valid_tgt_dir = "./ours_clean_nero_test/test_target.txt"
-    test_tgt_dir = "./ours_clean_nero_test/test_target.txt"
+    train_src_dir = "./dataset/train_source.txt"
+    valid_src_dir = "./dataset/test_source.txt"
+    test_src_dir = "./dataset/test_source.txt"
 
-    train_set = preprocessing(train_src_dir, train_tgt_dir, max_token_seq_len)
-    valid_set = preprocessing(valid_src_dir, valid_tgt_dir, max_token_seq_len)
-    test_set = preprocessing(test_src_dir, test_tgt_dir, max_token_seq_len)
+    train_tgt_dir = "./dataset/train_target.txt"
+    valid_tgt_dir = "./dataset/test_target.txt"
+    test_tgt_dir = "./dataset/test_target.txt"
 
-    train_set.to_json('ours_clean_nero_test/train.json', orient='records', lines=True)
-    valid_set.to_json('ours_clean_nero_test/valid.json', orient='records', lines=True)
-    test_set.to_json('ours_clean_nero_test/test.json', orient='records', lines=True)
-    '''
-    #train_src_dir = "./train_source.txt"
-    #valid_src_dir = "./test_source.txt"
-    test_src_dir = "./bpe_nero_test_source.txt"
+    train_set = preprocessing(train_src_dir, train_tgt_dir, opt.max_token_seq_len)
+    valid_set = preprocessing(valid_src_dir, valid_tgt_dir, opt.max_token_seq_len)
+    test_set = preprocessing(test_src_dir, test_tgt_dir, opt.max_token_seq_len)
 
-    #train_tgt_dir = "./train_target.txt"
-    #valid_tgt_dir = "./test_target.txt"
-    test_tgt_dir = "./nero_test_target.txt"
+    train_set.to_json('dataset/train.json', orient='records', lines=True)
+    valid_set.to_json('dataset/valid.json', orient='records', lines=True)
+    test_set.to_json('dataset/test.json', orient='records', lines=True)
 
-    #train_set = preprocessing(train_src_dir, train_tgt_dir, max_token_seq_len)
-    #valid_set = preprocessing(valid_src_dir, valid_tgt_dir, max_token_seq_len)
-    test_set = preprocessing(test_src_dir, test_tgt_dir, max_token_seq_len)
-
-    #train_set.to_json('ours_clean_nero_test/train.json', orient='records', lines=True)
-    #valid_set.to_json('ours_clean_nero_test/valid.json', orient='records', lines=True)
-    test_set.to_json('./nero_test.json', orient='records', lines=True)
-    '''
     ## tokenize
     global tokenize
+    global code
+    global text
     tokenize = lambda x : x.split()
 
     code = Field(sequential=True, 
@@ -356,7 +377,7 @@ if __name__ == '__main__':
                 tokenize=tokenize, 
                 lower=True,
                 pad_token='<pad>',
-                fix_length=max_token_seq_len)
+                fix_length=opt.max_token_seq_len)
 
     text = Field(sequential=True, 
                 use_vocab=True, 
@@ -365,14 +386,14 @@ if __name__ == '__main__':
                 init_token='<sos>',
                 eos_token='<eos>',
                 pad_token='<pad>',
-                fix_length=max_token_seq_len)
+                fix_length=opt.max_token_seq_len)
 
     fields = {'Code' : ('code', code), 'Text' : ('text', text)}
 
     train_data, valid_data, test_data = TabularDataset.splits(path='',
-                                                train='./ours_clean_nero_test/train.json',
-                                                test='./ours_clean_nero_test/test.json',
-                                                validation='./ours_clean_nero_test/valid.json',
+                                                train='./dataset/train.json',
+                                                test='./dataset/test.json',
+                                                validation='./dataset/valid.json',
                                                 format='json',
                                                 fields=fields)
 
@@ -381,8 +402,8 @@ if __name__ == '__main__':
     
     train_iterator, valid_iterator, test_iterator = BucketIterator.splits(
         (train_data, valid_data, test_data),
-        batch_size=batch_size,
-        device = "cuda",
+        batch_size=opt.batch_size,
+        device = device,
         sort=False
     )
 
@@ -395,32 +416,32 @@ if __name__ == '__main__':
                                     trg_vocab_size,
                                     src_pad_idx=src_pad_idx,
                                     trg_pad_idx=trg_pad_idx,
-                                    trg_emb_prj_weight_sharing=proj_share_weight,
-                                    emb_src_trg_weight_sharing=embs_share_weight,
-                                    d_k=d_k,
-                                    d_v=d_v,
-                                    d_model=d_model,
-                                    d_word_vec=d_word_vec,
-                                    d_inner=d_inner_hid,
-                                    n_layers=n_layers,
-                                    n_head=n_head,
-                                    dropout=dropout,
-                                    scale_emb_or_prj=scale_emb_or_prj,
-                                    n_position=max_token_seq_len+3).to(device)
+                                    trg_emb_prj_weight_sharing=opt.proj_share_weight,
+                                    emb_src_trg_weight_sharing=opt.embs_share_weight,
+                                    d_k=opt.d_k,
+                                    d_v=opt.d_v,
+                                    d_model=opt.d_model,
+                                    d_word_vec=opt.d_word_vec,
+                                    d_inner=opt.d_inner_hid,
+                                    n_layers=opt.n_layers,
+                                    n_head=opt.n_head,
+                                    dropout=opt.dropout,
+                                    scale_emb_or_prj=opt.scale_emb_or_prj,
+                                    n_position=opt.max_token_seq_len+3).to(device)
 
     model = nn.DataParallel(model)
 
     model_optimizer = ScheduledOptim(
         optim.Adam(model.parameters(), betas=(0.9, 0.98), eps=1e-09),
-        lr_mul, d_model, n_warmup_steps)
+        opt.lr_mul, opt.d_model, opt.n_warmup_steps)
     
     # Train
-    trainIters(model, n_epoch, train_iterator, valid_iterator, model_optimizer, smoothing)
+    trainIters(model, opt.n_epoch, train_iterator, valid_iterator, model_optimizer, opt.smoothing, trg_pad_idx)
     writer.close()
 
     # read json files made from torchtext
     test_data = list()
-    for line in open("./ours_clean_nero_test/test.json", mode='r', encoding='utf-8'):
+    for line in open("./dataset/test.json", mode='r', encoding='utf-8'):
         test_data.append(json.loads(line))
 
     test_src = list()
@@ -441,3 +462,6 @@ if __name__ == '__main__':
     with open('./predicted_output/ground_truth.txt', mode="w", encoding="UTF-8", errors='ignore') as out:
         for refer in reference_list:
             out.write(refer+'\n')
+
+if __name__ == '__main__':
+    main()
